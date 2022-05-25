77f2d8fa35df3659d77ca2a92fa58bf3fe6fb255
Mykyta Ryasny <MykytaProject@gmail.com>
2022-05-25 17:12:12 +0200

    JavaDoc
    Último JavaDoc creado

--- doc/member-search-index.js
+++ doc/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"sumador","c":"ASumar","l":"ASumar()","u":"%3Cinit%3E()"},{"p":"sumador","c":"ASumar","l":"sumativo(String)","u":"sumativo(java.lang.String)"}];updateSearchResults();
+memberSearchIndex = [{"p":"sumador","c":"ASumar","l":"ASumar()","u":"%3Cinit%3E()"},{"p":"sumador","c":"ASumar","l":"negativo(String)","u":"negativo(java.lang.String)"},{"p":"sumador","c":"ASumar","l":"sumativo(String)","u":"sumativo(java.lang.String)"},{"p":"sumador","c":"ASumar","l":"total(String)","u":"total(java.lang.String)"}];updateSearchResults();
